baa6fc0b34aff82b6704ea78b31a9afd1a7260ca
devil <562157205@qq.com>
2023-08-28 14:41:19 +0800

    feat: nacos配置刷新实验

---
+++ service-modules/example/example-service/src/main/java/com/devil/fission/machine/example/service/service/NacosFlagService.java
@@ -0,0 +1,27 @@
+package com.devil.fission.machine.example.service.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Service;
+
+/**
+ * NacosFlagService.
+ *
+ * @author Devil
+ * @date Created in 2023/8/28 14:38
+ */
+@RefreshScope
+@Service
+public class NacosFlagService {
+    
+    @Value("${example.configFlag:void}")
+    private String configFlag;
+    
+    /**
+     * nacos配置刷新实验.
+     */
+    public String flag() {
+        return configFlag;
+    }
+    
+}
