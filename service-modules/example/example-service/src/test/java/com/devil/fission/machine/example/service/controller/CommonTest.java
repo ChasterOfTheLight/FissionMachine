60f2d16c0dfbdba891568307af0d8a2325251f85
devil <562157205@qq.com>
2024-05-08 10:48:39 +0800

    feat: json对象合并单元测试

--- service-modules/example/example-service/src/test/java/com/devil/fission/machine/example/service/controller/CommonTest.java
+++ service-modules/example/example-service/src/test/java/com/devil/fission/machine/example/service/controller/CommonTest.java
@@ -1,6 +1,10 @@
 package com.devil.fission.machine.example.service.controller;
 
+import cn.hutool.json.JSONConfig;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.devil.fission.machine.common.response.ResponseCode;
+import com.devil.fission.machine.common.util.StringUtils;
 import com.github.benmanes.caffeine.cache.CacheLoader;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.Expiry;
@@ -24,7 +28,7 @@ import java.util.stream.Collectors;
  * @author Devil
  * @date Created in 2023/9/5 13:33
  */
-public class SysUserWebControllerTest {
+public class CommonTest {
     
     @Test
     public void enumTest() {
@@ -78,6 +82,23 @@ public class SysUserWebControllerTest {
         for (Long l : list) {
             System.out.println(l);
         }
+    }
+    
+    @Test
+    public void testJsonObject() {
+        String jsonOne = "{\"id\":\"1\",\"name\":\"test\",\"age\":\"56\",\"address\":\"77778888\"}";
+        String jsonTwo = "{\"id\":\"2\",\"name\":\"qwe\",\"age\":\"\",\"address\":\"\"}";
+        JSONObject jsonObjectOne = JSONUtil.parseObj(jsonOne);
+        JSONObject jsonObjectTwo = JSONUtil.parseObj(jsonTwo, JSONConfig.create().setIgnoreNullValue(true));
+        for (String key : jsonObjectOne.keySet()) {
+            if (jsonObjectOne.containsKey(key) && jsonObjectTwo.containsKey(key)) {
+                if (jsonObjectTwo.get(key) == null || StringUtils.isEmpty(jsonObjectTwo.getStr(key))) {
+                    jsonObjectTwo.set(key, jsonObjectOne.get(key));
+                }
+            }
+        }
+        System.out.println(JSONUtil.toJsonStr(jsonObjectOne));
+        System.out.println(jsonObjectTwo.toString());
     }
     
 }
